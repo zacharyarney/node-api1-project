c41a4f078774ac251a0bad43d57d7853b9fa4be4
zacharyarney <zachary.arney@gmail.com>
2019-09-16 16:34:29 -0700

    Adds GET and POST endpoints for '/api/users'

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 const express = require('express');
 
+const db = require('./data/db.js');
+
 const server = express();
 
 // MIDDLEWARE
 server.use(express.json());
 
 // ENDPOINTS
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then((users) => res.status(200).send(users))
+    .catch((err) => {
+      res.status(500).json({
+        message: 'could not find users',
+        err,
+      });
+    });
+});
 
+server.post('/api/users', (req, res) => {
+  const newUser = req.body;
+  db.instert(newUser)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: 'please provide all necessary info',
+        err,
+      });
+    });
+});
 
 // EXPORT
 module.exports = server;
